a6cd7e139cf5510af3e0e1ab94caed476105581e
Sohamraje137 <sohampatil798@gmail.com>
2018-12-14 16:42:55 +0530

    Added changes

--- users/models.py
+++ users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here
 class UserInfo(models.Model):
+    # userforeignkey = models.OneToOneField(User, on_delete=models.CASCADE)
     user_name = models.CharField(u'username',max_length=15,default='')
     user_phone =  models.CharField(u'cellphone number',max_length=10,default='')
     user_first_name = models.CharField(u'Enter first name',max_length=15,default='Chaman')
